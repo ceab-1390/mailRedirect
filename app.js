d118c7ec1ec65010e24814b7aa1c9220d8e26586
Cesar Avila <cesar.e.avila.b@gmail.com>
2024-04-23 15:01:53 -0400

    Fix: se corrigio el reemvio del mismo item varias veces

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const {bot} = require('./bot');
 const TLS = process.env.IMAP_TLS
 const TLS_VAL = TLS === 'true';
 //|(VPTI-\d+)
-const regex = /(Error)|(Falla)|(Incidencia)/i;
+const regex = /(Error)|(Falla)|(Incidencia)|(VPTI-\d+)|(Invitacion)|(Reunion)/i;
 
 let mails = [];
 const imapConfig = {
@@ -27,62 +27,65 @@ imap.once('ready', function() {
     imap.openBox('INBOX', true, function(err, box) {
         if (err) throw err;
         Logguer.log('Buzón abierto: ' + box.name);
-        const today = moment().format('MMM DD, YYYY');
-        Logguer.log(today);
-            imap.on('mail', function(){
-                // Buscar correos no leídos de hoy
+            imap.on('mail', (cant)=>{
+                const today = moment().format('MMM DD, YYYY');
+                Logguer.log(today);
+                let nuevos = cant;
                 const searchCriteria = ['UNSEEN',['SINCE', today]];
                 const fetchOptions = { bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)', ''], struct: true };
-
                 imap.search(searchCriteria, function(err, results) {
                     if (err) throw err;
-
-                    // Fetch los correos encontrados
-                    const f = imap.fetch(results, fetchOptions);
-                    f.on('message', function(msg, seqno) {
-                        msg.on('body',  function(stream, info) {
-                            simpleParser(stream, {}, (err, mail) => {
-                                //Logguer.log(mail.headerLines)
-                                if (err) throw err;
-                                mails[seqno] = {}
-                                mails[seqno].from = mail.from.text;
-                                mails[seqno].subject = mail.subject;
-                                mails[seqno].date = mail.date;
-                                if (mail.text) {
-                                    mails[seqno].text = mail.text;
-                                }else if (mail.html){
-                                    mails[seqno].text = mail.html
-                                }
+                    results = results.sort((a,b) => b - a );
+                    for ( let i = 0; i < nuevos; i++ ){
+                        const f = imap.fetch(results[i], fetchOptions);
+                        f.on('message', function(msg, seqno) {
+                            msg.on('body',  function(stream, info) {
+                                simpleParser(stream, {}, (err, mail) => {
+                                    if (err) throw err;
+                                    mails[seqno] = {}
+                                    mails[seqno].from = mail.from.text;
+                                    mails[seqno].subject = mail.subject;
+                                    mails[seqno].date = mail.date;
+                                    mails[seqno].id = results[i];
+                                    if (mail.text) {
+                                        mails[seqno].text = mail.text;
+                                    }else if (mail.html){
+                                        mails[seqno].text = mail.html
+                                    }
+                                });
                             });
                         });
-                    });
-                    f.once('error', function(err) {
-                        Logguer.log('Error al fetch: ' + err);
-                    });
-                    f.once('end', function() {
-                        Logguer.log('Fin de fetch');
-                        //let filterMail = mails.filter(mailfilter => regex.test(mailfilter.subject));
-                        let Filter = mails.filter(mail_f => {
-                            const match = mail_f.subject.match(regex);
-                            return match !== null;
+                        f.once('error', function(err) {
+                            Logguer.log('Error al fetch: ' + err);
+                        });
+                        f.once('end', function() {
+                            Logguer.log('Fin de fetch');
+                            let Filter = mails.filter(mail_f => {
+                                const match = mail_f.subject.match(regex);
+                                return match !== null;
+                            });
+    
+                            Filter.forEach(async mail =>{
+                                    const match = mail.subject.match(regex);
+                                    const tipo = match[1] ? 'Error' : match[2] ? 'Falla' : match[3] ? 'Incidencia' : match[4] ? 'VPTI' : match[5] ? 'Invitacion' : match[6] ? 'Reunion' : Logguer.info('No se encontro coincidencias con los parametros de busqueda...')
+                                    Logguer.log(mail.id+' '+mail.date);
+                                    try {
+                                        await bot.telegram.sendMessage('854982095', 'Grupo: '+tipo+' \n\nAsunto: '+mail.subject+' \n\nContenido:\n\n'+mail.text);
+                                        imap.addFlags(mail.id, ['\\SEEN'], function(err) {
+                                            if (err) {
+                                                Logguer.error('Error al marcar el correo como leído: ' + err);
+                                            } else {
+                                                Logguer.log('Correo marcado como leído: ' + mail.subject);
+                                            }
+                                        Logguer.log('Send')
+                                        });
+                                    } catch (error) {
+                                        Logguer.error(error)
+                                    }
+                            })
                         });
 
-                        Filter.forEach(async mail =>{
-                            const match = mail.subject.match(regex);
-                            //Logguer.log(match)//match[3] ? 'VPTI' :
-                                const tipo = match[1] ? 'Error' : match[2] ? 'Falla' : match[3] ? 'Incidencia' : Logguer.log('...')
-                                // Logguer.log('================================================================================')
-                                // Logguer.log('Grupo: '+tipo);
-                                // Logguer.log(mail.subject);
-                                // Logguer.log(mail.text)
-                                Logguer.log('Send')
-                                try {
-                                    await bot.telegram.sendMessage('854982095', 'Grupo: '+tipo+' \n\nAsunto: '+mail.subject+' \n\nContenido:\n\n'+mail.text);
-                                } catch (error) {
-                                    Logguer.error(error)
-                                }
-                        })
-                    });
+                    };
                 });
             });
     });
